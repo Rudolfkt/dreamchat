5bba7adde35817c94e398393708074040ec0ce12
Rudolf Tetteh <105998416+Rudolfkt@users.noreply.github.com>
2023-08-18 15:45:24 -0600

    update

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -1,16 +1,14 @@
-import React from 'react'; 'react';
-import * as firebase from './firebase';
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdrvpAq1iFak--hJ-_IENrLlwOt4IQaVg",
-    authDomain: "dreamchat-6b8f2.firebaseapp.com",
-    projectId: "dreamchat-6b8f2",
-    storageBucket: "dreamchat-6b8f2.appspot.com",
-    messagingSenderId: "846461310426",
-    appId: "1:846461310426:web:2509a26c3e625d8f6e5ce3"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAdrvpAq1iFak--hJ-_IENrLlwOt4IQaVg",
+  authDomain: "dreamchat-6b8f2.firebaseapp.com",
+  projectId: "dreamchat-6b8f2",
+  storageBucket: "dreamchat-6b8f2.appspot.com",
+  messagingSenderId: "846461310426",
+  appId: "1:846461310426:web:2509a26c3e625d8f6e5ce3"
+};
 
-  export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
