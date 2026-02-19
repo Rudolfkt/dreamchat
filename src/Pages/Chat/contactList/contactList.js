305042a23d45fbabee7c857b1f8dd76012d72898
Rudolf Tetteh <rudolftetteh16@gmail.com>
2026-02-19 06:49:51 +0000

    options ui complete2

--- src/Pages/Chat/contactList/contactList.js
+++ src/Pages/Chat/contactList/contactList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import UserProfileCard from './Profile/UserProfileCard';
 import './contactList.css';
 import { Avatar } from '@mui/material';
