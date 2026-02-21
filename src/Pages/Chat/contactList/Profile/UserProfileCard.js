8657ebc4970f6c5bb43b95907aca86cd6784a326
Rudolf Tetteh <rudolftetteh16@gmail.com>
2026-02-21 00:31:02 +0000

    userProfileCard ui update

--- src/Pages/Chat/contactList/Profile/UserProfileCard.js
+++ src/Pages/Chat/contactList/Profile/UserProfileCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './UserProfileCard.css';
+import { Avatar } from '@mui/material';
 
 const UserProfileCard = ({ contact }) => {
     return (
         <div className="user-profile-card">
-            <h3>{contact.name}</h3>
-            <p>Last Message: {contact.lastMessage}</p>
-            <button className="profile-action">Message</button>
-            <button className="profile-action">Block</button>
+            <button className="profile-photo-card">
+                <Avatar
+                    className="profile-avatar"
+                    src="https://ui-avatars.com/api/?name=Rudolf&size=45&background=1ebea5&color=fff"
+                    alt="Profile"
+                    sx={{ width: 40, height: 40 }}
+                />
+                <p className='name'>Rudolf</p>
+                <p className='bio'>Ad Astra</p>
+            </button>
+            <button className="profile-action">Account</button>
+            <button className="profile-action">Chats</button>
+            <button className="profile-action">Wallet</button>
+            <button className="profile-action">Saved Messages</button>
+            <button className="profile-action">Recent Calls</button>
+            <button className="profile-action">Linked Devices</button>
+            <button className="profile-action">Themes</button>
+            <button className="help">Help</button>
+
+            
         </div>
     );
 };
